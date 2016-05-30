b173437fc259550025e1051fd43da7f0b7a8e410
Alquimista <alquimista@gmx.pt>
2016-05-30 22:20:05 +0100

    Update install_memcached.sh

--- install_memcached.sh
+++ install_memcached.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 git clone https://github.com/php-memcached-dev/php-memcached
 cd php-memcached
 git checkout -b php7 origin/php7
 /usr/local/php7/bin/phpize
 ./configure --with-php-config=/usr/local/php7/bin/php-config
-make install
+make
+sudo make install
