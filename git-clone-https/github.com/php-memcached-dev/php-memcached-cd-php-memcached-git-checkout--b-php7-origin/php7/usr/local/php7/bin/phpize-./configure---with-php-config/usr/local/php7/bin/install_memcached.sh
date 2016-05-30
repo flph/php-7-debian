b5e555e7f0e2da4a122033da409ea066ad98a45a
Alquimista <alquimista@gmx.pt>
2016-05-30 22:17:37 +0100

    Delete install_memcached.sh

--- git-clone-https/github.com/php-memcached-dev/php-memcached-cd-php-memcached-git-checkout--b-php7-origin/php7/usr/local/php7/bin/phpize-./configure---with-php-config/usr/local/php7/bin/install_memcached.sh
+++
@@ -1,6 +0,0 @@
-git clone https://github.com/php-memcached-dev/php-memcached
-cd php-memcached
-git checkout -b php7 origin/php7
-/usr/local/php7/bin/phpize
-./configure --with-php-config=/usr/local/php7/bin/php-config
-make install
