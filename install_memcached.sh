d3835d954255cc2c3970a4abf8e55274f5234d40
Alquimista <alquimista@gmx.pt>
2016-05-30 22:26:47 +0100

    Update install_memcached.sh

--- install_memcached.sh
+++ install_memcached.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
+sudo apt-get update
+sudo apt-get install -y \
+    libmemcached-dev \
+    libmemcached11 
+
 git clone https://github.com/php-memcached-dev/php-memcached
 cd php-memcached
 git checkout -b php7 origin/php7
